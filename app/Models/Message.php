34d969ed00fa0146e7d1464d4fa3d2f3ed2cbb03
biniyam yoseph <biniyamyoseph063@gmail.com>
2024-05-27 18:27:04 +0300

    model relation and factory

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'message',
+        'sender_id',
+        'group_id',
+        'receiver_id',
+    ];
+
+    public function sender()
+    {
+        return $this->belongsToMany(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsToMany(User::class, 'receiver_id');
+    }
+
+    public function group()
+    {
+        return $this->belongsToMany(Group::class);
+    }
+    public function attachments()
+    {
+        return $this->hasMany(MessageAttachment::class);
+    }
 }
